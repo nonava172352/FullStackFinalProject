e2a7fe4fb147d2779fc4d150f37f6fe790c4c232
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2022-05-20 16:05:43 +0700

    First

---
+++ frontend/src/route/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: () => import('../views/Homepage.vue')
+    },
+    {
+        path: '/postpage',
+        name: 'post',
+        component: () => import('../views/Postpage.vue')
+    }
+    
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
---
+++ frontend/src/views/Postpage.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Postpage</h1>
+</template>
