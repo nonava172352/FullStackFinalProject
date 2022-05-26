16366579f02aea168a515fe981eed737b2b8063f
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2022-05-26 19:14:12 +0700

    Update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,12 +4,15 @@ import router from './route'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'bootstrap'; import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createProvider } from './vue-apollo'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App),
+  apolloProvider: createProvider(),
+  render: h => h(App)
 }).$mount('#app')
