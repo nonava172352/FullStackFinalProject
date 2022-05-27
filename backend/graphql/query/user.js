9adc781a6453f98d9e0e6b19678ded85df9927ce
Cheese kung <fookkub1@gmail.com>
2022-05-27 16:25:53 +0700

    :sparkles: add search user

--- backend/graphql/query/user.js
+++ backend/graphql/query/user.js
@@ -1,4 +1,29 @@
-import { UserTC } from "../../model/user"
+import { schemaComposer } from "graphql-compose"
+import { UserTC, UserModel } from "../../model/user"
 
 export const users = UserTC.mongooseResolvers.findMany()
 export const userId = UserTC.mongooseResolvers.findById()
+export const searchUser = schemaComposer.createResolver({
+    name: "searchUser",
+    kind: "query",
+    type: "[JSON]",
+    args: {
+        search: "String!"
+    },
+    resolve: async ({ args }) => {
+        const users = await UserModel.find()
+        const result = []
+
+        users.forEach(user => {
+            const fullname = user.firstname + user.lastname
+            
+            if (fullname.includes(args.search) || user.email.includes(args.search)) {
+                result.push(user)
+            } else if (user.type === "Student" && user.studentId.includes(args.search)) {
+                result.push(user)
+            }
+        })
+
+        return result
+    }
+})
