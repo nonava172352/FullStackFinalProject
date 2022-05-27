5df9f3c898d536bb3da86a1377366689ece2fa7e
Flukena <56498257+nonava172352@users.noreply.github.com>
2022-05-27 15:21:51 +0700

    dsadsa

--- frontend/src/views/Homepage.vue
+++ frontend/src/views/Homepage.vue
@@ -2,49 +2,28 @@
   <div class="container-fluid">
     <div class="row flex-nowrap">
       <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-        <div
-          class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100"
-        >
-          <ul
-            class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-            id="menu"
-          >
+        <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+          <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
             <li>
-              <a
-                href="#submenu1"
-                data-bs-toggle="collapse"
-                class="nav-link px-0 align-middle"
-              >
+              <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
                 <i class="fs-4 bi-speedometer2"></i>
-                <span class="ms-1 d-none d-sm-inline text-white"
-                  >รายชื่อนักศึกษาตามสาขา</span
-                >
+                <span class="ms-1 d-none d-sm-inline text-white">รายชื่อนักศึกษาตามสาขา</span>
               </a>
-              <ul
-                class="collapse show nav flex-column ms-1"
-                id="submenu1"
-                data-bs-parent="#menu"
-              >
+              <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
                 <li class="w-100">
                   <a href="/it" class="nav-link px-0">
-                    <span class="d-none d-sm-inline"
-                      >สาขาวิชาเทคโนโลยีสารสนเทศ (IT)</span
-                    >
+                    <span class="d-none d-sm-inline">สาขาวิชาเทคโนโลยีสารสนเทศ (IT)</span>
                   </a>
                 </li>
                 <li>
                   <a href="/bit" class="nav-link px-0">
-                    <span class="d-none d-sm-inline"
-                      >สาขาเทคโนโลยีสารสนเทศทางธุรกิจ (BIT)</span
-                    >
+                    <span class="d-none d-sm-inline">สาขาเทคโนโลยีสารสนเทศทางธุรกิจ (BIT)</span>
                   </a>
                 </li>
                 <li>
                   <a href="/datascience" class="nav-link px-0">
-                    <span class="d-none d-sm-inline"
-                      >สาขาวิชาวิทยาการข้อมูลและการวิเคราะห์เชิงธุรกิจ (Data
-                      Science)</span
-                    >
+                    <span class="d-none d-sm-inline">สาขาวิชาวิทยาการข้อมูลและการวิเคราะห์เชิงธุรกิจ (Data
+                      Science)</span>
                   </a>
                 </li>
               </ul>
@@ -52,9 +31,7 @@
             <li>
               <a href="/lecturer" class="nav-link px-0 align-middle">
                 <i class="fs-4 bi-people"></i>
-                <span class="ms-1 d-none d-sm-inline text-white"
-                  >รายชื่ออาจารย์</span
-                >
+                <span class="ms-1 d-none d-sm-inline text-white">รายชื่ออาจารย์</span>
               </a>
             </li>
           </ul>
@@ -66,174 +43,122 @@
         <div class="position">
           <form class="d-flex" style="width: 45%; margin-top: 2%">
             <button class="btn btn-success me-2" type="submit">Search</button>
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="ค้นห้าโพสต์ที่เกี่ยวข้อง"
-              aria-label="Search"
-            />
+            <input class="form-control me-2" type="search" placeholder="ค้นห้าโพสต์ที่เกี่ยวข้อง" aria-label="Search" />
             <button class="btn btn-warning" type="submit" style="width: 200px">
               เขียนโพสต์
             </button>
           </form>
         </div>
 
-        <!-- <div v-if="search != ''"> -->
-        <!-- <div v-for="(post, index) in filtereListd" :key="index"> -->
-        <div
-          class="modal fade"
-          id="BlogModal"
-          tabindex="-1"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
+        <div class="modal fade" id="BlogModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog" style="max-width: 45%">
             <div class="modal-content">
               <div class="modal-header">
-                <h5
-                class="card-title"
-                id="contcolor"
-                style="
-                  display: flex;
-                  align-items: start;
-                  justify-content: start;
-                  font-weight: 900;
-                "
-              >
-                Blog.title
-              </h5>
-                <button
-                  type="button"
-                  class="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
+                <h5 class="card-title" id="contcolor" style="
+                    display: flex;
+                    align-items: start;
+                    justify-content: start;
+                    font-weight: 900;
+                  ">
+                  {{ post.title }}
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
                 <form>
                   <div class="mb-3 justify-content-end">
-                    <p
-                class="card-text text-left"
-                style="
-                  display: flex;
-                  align-items: start;
-                  justify-content: start;
-                "
-                id="contcolor2"
-              >
-                Blog.content
-              </p>
-              <p style="
-                  display: flex;
-                  align-items: start;
-                  justify-content: start;
-                  margin-left: 2%;
-                "
-                >ผู้เขียน Blog.email</p>
-              <!-- ทำลูปคอมเเม้นตรงนี้ ข้างล่าง-->
-              <div id="contcolor3">
-                <p style="
-                  display: flex;
-                  align-items: start;
-                  justify-content: start;
-                "><button type="button" class="btn-close btn-close-white" aria-label="Close" style="
-                  display: flex;
-                  align-items: start;
-                  justify-content: start;"></button> ผู้เขียน Blog.email.comment</p>
-                
-              <p
-                class="card-text d-flex"
-                style="
-                  display: flex;
-                  align-items: end;
-                  justify-content: end;
-                "
-              >
-                Blog.comment
-              </p>
-              </div>
-              <!-- ทำลูปคอมเเม้นตรงนี้ ข้างบน-->
-                  
-                  </div>
+                    <p class="card-text text-left" style="
+                        display: flex;
+                        align-items: start;
+                        justify-content: start;
+                      " id="contcolor2">
+                      {{ post.content }}
+                    </p>
+                    <p style="
+                        display: flex;
+                        align-items: start;
+                        justify-content: start;
+                        margin-left: 2%;
+                      ">
+                      {{ post.create_by_email }}
+                    </p>
 
+
+                    <div id="contcolor3" v-for="(comment, index ) in comments " :key='index'>
+                      <p style="
+                          display: flex;
+                          align-items: start;
+                          justify-content: start;
+                        ">
+                        <button type="button" @click="deleteComment(comment)" class="btn-close btn-close-white"
+                          aria-label="Close" style="
+                            display: flex;
+                            align-items: start;
+                            justify-content: start;
+                          "></button>
+                        {{ comment.comment_by_email }}
+                      </p>
+                      <p class="card-text d-flex" style="
+                          display: flex;
+                          align-items: end;
+                          justify-content: end;
+                        ">
+                        {{ comment.comment }}
+                      </p>
+                    </div>
+                  </div>
                 </form>
               </div>
               <div class="modal-footer justify-content-center">
                 <div style="width: 100%">
-                <form class="d-flex" style="width: 100%">
-                  <input
-                    class="form-control me-2"
-                    type="input"
-                    placeholder="เขียนความคิดเห็น"
-                    aria-label="Search"
-                  />
-                  <button
-                    class="btn btn-success"
-                    type="submit"
-                    style="width: 100px"
-                  >
-                    ตกลง
-                  </button>
-                </form>
+                  <div class="d-flex" style="width: 100%">
+                    <input class="form-control me-2" type="input" v-model="comment" placeholder="เขียนความคิดเห็น"
+                      aria-label="Search" />
+                    <button class="btn btn-success" type="submit" style="width: 100px" @click="addComment()">
+                      ตกลง
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="position">
-          <div class="card" style="width: 45%; margin-top: 2%">
-            <img
-                  class="card-img-top"
-                  src="../assets/testpic.jpg"
-                  alt="Card image cap"
-                  style="max-width: 100%"
-                />
-            <div class="card-body">
-              <h5
-                class="card-title"
-                id="contcolor"
-                style="
-                  display: flex;
-                  align-items: start;
-                  justify-content: start;
-                  font-weight: 900;
-                "
-              >
-                Blog.title
-              </h5>
-              <p
-                class="card-text text-left"
-                style="
-                  display: flex;
-                  align-items: start;
-                  justify-content: start;
-                "
-                id="contcolor2"
-              >
-                Blog.content
-              </p>
-              <div
-                class="d-flex justify-content-between"
-              >
-                <button type="button" class="btn btn-outline-danger">ลบโพสต์</button>
-                <a
-                  href="BlogModal"
-                  class="btn btn-light"
-                  @click="getBlog(post)"
-                  data-bs-toggle="modal"
-                  data-bs-target="#BlogModal"
-                  >เเสดงความคิดเห็น</a
-                >
-                
+
+        <!-- <div v-if="search != ''"> -->
+        <div v-for="(Blog, index) in Blogs" :key="index">
+          <div class="position">
+            <div class="card" style="width: 45%; margin-top: 2%">
+              <img class="card-img-top" src="../assets/testpic.jpg" alt="Card image cap" style="max-width: 100%" />
+              <div class="card-body">
+                <h5 class="card-title" id="contcolor" style="
+                    display: flex;
+                    align-items: start;
+                    justify-content: start;
+                    font-weight: 900;
+                  ">
+                  {{ Blog.title }}
+                </h5>
+                <p class="card-text text-left" style="
+                    display: flex;
+                    align-items: start;
+                    justify-content: start;
+                  " id="contcolor2">
+                  {{ Blog.content }}
+                </p>
+                <div class="d-flex justify-content-between">
+                  <button type="button" class="btn btn-outline-danger">
+                    ลบโพสต์
+                  </button>
+                  <a href="BlogModal" class="btn btn-light" @click="getComment(Blog)" data-bs-toggle="modal"
+                    data-bs-target="#BlogModal">เเสดงความคิดเห็น</a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <!-- </div> -->
       </div>
     </div>
   </div>
-  <!-- </div> -->
 </template>
 
 <style>
@@ -242,6 +167,7 @@
   align-items: center;
   justify-content: center;
 }
+
 #contcolor {
   background-color: rgb(228, 228, 228);
   /* border: 0px solid rgb(0, 0, 0); */
@@ -267,31 +193,139 @@
 }
 
 #bgcolor {
-  background-image: linear-gradient(
-    to left,
-    rgb(255, 255, 255),
-    rgb(206, 231, 255)
-  );
+  background-image: linear-gradient(to left,
+      rgb(255, 255, 255),
+      rgb(206, 231, 255));
 }
 </style>
 
 <script>
 import axios from "axios";
+import { jwtDecode } from "../jwt-decode";
 export default {
   data() {
     return {
-      Blogs: []
+      Blogs: [],
+      user: [],
+      comments: [],
+      post: [],
+      comment: "",
+      token: ""
     };
-  },methods: {
-    getblog(){
-      axios.get(process.env.VUE_APP_HOST+ `blog`).then((res) => {
-        this.Blogs = res.data;
-        console.log(this.Blogs)
-      }).catch((error) => console.log(error))
+  },
+  methods: {
+    getblog() {
+      axios
+        .get(process.env.VUE_APP_HOST + `blog`)
+        .then((res) => {
+          this.Blogs = res.data;
+        })
+        .catch((error) => console.log(error));
+    },
+    checklogin() {
+      console.log("test")
+      if (this.$cookies.get("refresh_token") != null) {
+
+        axios
+          .get(process.env.VUE_APP_HOST + "auth/refresh_token", {
+            headers: {
+              Authorization: `Bearer ${this.$cookies.get("refresh_token")}`,
+            },
+          })
+          .then((res) => {
+            if (res.status == 200) {
+              this.$accessToken = res.data.accessToken;
+              this.$cookies.set("refresh_token", res.data.refreshToken);
+              this.token = res.data.accessToken;
+              this.$users = jwtDecode(res.data.refreshToken);
+              this.user = jwtDecode(res.data.refreshToken);
+              this.email = this.user.email
+              console.log(this.$users)
+              this.$router.push({ name: 'home', query: { redirect: '/' } })
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        console.log("ยังไม่ได้ login")
+        this.$router.push({ name: 'login', query: { redirect: '/login' } })
+      }
+    },
+    getComment(blog) {
+      this.post = blog
+      axios
+        .get(process.env.VUE_APP_HOST + `comment/` + blog.blog_id)
+        .then((res) => {
+          this.comments = res.data;
+        });
+    }, addComment() {
+
+      this.checklogin();
+      if (this.comment == "") {
+        alert("กรุณาพิมพ์ข้อความ");
+        return;
+      } else {
+        this.user = jwtDecode(this.token)
+        if (this.$cookies.get('refresh_token') != null) {
+          this.user = jwtDecode(this.$cookies.get('refresh_token'))
+
+        }
+        if (this.token != null) {
+          axios
+            .post(
+              process.env.VUE_APP_HOST + "comment/create",
+              {
+                blog_id: this.post.blog_id,
+                comment_by_email: this.user.email,
+                comment_by_firstname: this.user.firstname,
+                comment: this.comment,
+              },
+              {
+                headers: {
+                  Authorization: `Bearer ${this.token}`,
+                },
+              }
+            )
+            .then((res) => {
+              if (res) {
+                axios
+                  .get(process.env.VUE_APP_HOST + `comment/` + this.post.blog_id)
+                  .then((res) => {
+                    this.comments = res.data;
+                  });
+                alert("บันทึกข้อความสำเร็จ");
+      this.comment = ''
+
+              }
+            })
+            .catch((error) => {
+              alert(error);
+              console.log(error);
+            });
+        } else {
+          alert("กรุณา login")
+        }
+      }
+    }, deleteComment(comment) {
+      axios.delete(process.env.VUE_APP_HOST + 'comment/' + comment.comment_id + '/' + this.user.email, {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      }).then((res) => {
+        alert(res.data)
+        axios
+          .get(process.env.VUE_APP_HOST + `comment/` + this.post.blog_id)
+          .then((res) => {
+            this.comments = res.data;
+          });
+      }).catch(error => alert("ไม่สามารถลบได้"))
     }
-  },created(){
-    this.getblog()
+  },
+  created() {
+    this.getblog();
+  }, updated() {
+    this.checklogin()
   }
-  
 };
 </script>
